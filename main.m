7a5a38a5b430400ab0b4c9211848be5bd7fdc45b
Jacob Killelea <Jkillelea@protonmail.ch>
2017-09-25 09:07:10 -0600

    differentiate between cylinder and airfoil datasets, smarter plotting and error handling

--- main.m
+++ main.m
@@ -1,26 +1,42 @@
 clear all; clc;
 
+% file descriptors for the console
+STDOUT = 1;
+STDERR = 2;
+
 % get filenames
-files = dir('../data/*.csv');
-n_files = length(files);
+cylinders = dir('../data/*Cylinder*.csv');
+airfoils  = dir('../data/*Airfoil*.csv');
 
-% xmat = zeros(20, n_files);
-% ymat = zeros(20, n_files);
-% qmat = zeros(20, n_files);
+n_airfoils = length(airfoils);
 
-% figure; hold on;
-for i = 1:n_files
-  filename = files(i).name;
-  data     = load_csv(['../data/' filename], 1, 0);
+fig_v15 = figure; hold on;
+title('15 m/s');
+fig_v25 = figure; hold on;
+title('25 m/s');
+
+for i = 1:length(cylinders)
+  % load in the file (with error handling)
+  filename = cylinders(i).name;
+  try
+    data = load_csv(['../data/' filename], 1, 0);
+  catch exception
+    fprintf('[WARNING] Unable to parse file %s. Continuing...\n', filename);
+  end
+
+  % determine which plot the data belongs on (with error handling)
+  if round(mean(data.airspeed)) == 15
+    set(0, 'currentfigure', fig_v15); % set the plot for 15 m/s to current
+  elseif round(mean(data.airspeed)) == 25
+    set(0, 'currentfigure', fig_v25); % set the plot for 25 m/s to current
+  else
+    fprintf('[WARNING] Expected airspeed of either 15 or 25, found %d in file %s.\n', ...
+     round(mean(data.airspeed)), filename);
+  end
 
   x = data.probe_x;
   y = data.probe_y;
   q = data.aux_dynamic_pressure;
-  % scatter3(x, y, q);
 
-  % xmat(:, i) = x;
-  % ymat(:, i) = y;
-  % qmat(:, i) = q;
+  scatter3(x, y, q);
 end
-
-% plot3(xmat, ymat, qmat);
