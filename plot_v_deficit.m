31cd9c9a298809b1c20d23d3017c26ab4e96dc3a
Jacob Killelea <Jkillelea@protonmail.ch>
2017-09-29 16:03:41 -0600

    Extra comments and change a fprintf into a warning

--- plot_v_deficit.m
+++ plot_v_deficit.m
@@ -11,20 +11,22 @@ for i = 1:length(files)
   try
     data = load_csv(['../data/', filename], 1, 0);
   catch err
-    fprintf('[WARNING] Unable to parse file %s. Continuing...\n', filename);
+    warning('Unable to parse file %s. Continuing...\n', filename);
     disp(err.message);
     continue
   end
 
+  % pull out the requisite data
   airspeed = mean(data.airspeed);
   rho      = mean(data.atmo_density);
   xpos     = mean(data.probe_x);
 
+  % calculate velocity deficit
   q = data.aux_dynamic_pressure;
   v = sqrt(2.*q/rho);
-
   deficit = airspeed - v;
 
+  % Generate a title
   titlestr = '';
   if contains(filename, 'Cylinder')
     titlestr = sprintf('%s - x = %.2f mm, v = %.2f m/s', 'Cylinder', xpos, airspeed);
@@ -32,13 +34,14 @@ for i = 1:length(files)
     titlestr = sprintf('%s - x = %.2f mm, v = %.2f m/s', 'Airfoil', xpos, airspeed);
   end
 
-  f = figure;
+  % plot and save
+  f = figure; hold on; grid on;
   scatter(deficit, data.probe_y);
-  grid on;
+
   title(titlestr);
   ylabel('y-axis (mm)');
   xlabel('Velocity deficit (m/s)');
 
   print(f, '-dpng', ['../graphs/', filename, '.png']);
-  close
+  close(f);
 end
